9d92e7402bbbd4978602cf493c9c00cacf78209b
khoivd <khoivd@sudobo.com>
2023-04-02 15:03:27 +0700

    add authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'mail.google.com',
+    user_name:            'vuduckhoia1@gmail.com',
+    password:             'pbltpetfxacdthsi',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5
+  }
+  
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout',
+        password: 'forgot-password', confirmation: 'verification', sign_up: 'sign_up' }
+
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
