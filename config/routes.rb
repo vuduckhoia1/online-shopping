38798a6f95aa6c0bd6680f28bcfbcec843bd42c3
khoivd <khoivd@sudobo.com>
2023-05-16 07:09:18 +0700

    ...

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users, except: %i[ new create edit ]
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout',
-        password: 'forgot-password', confirmation: 'verification', sign_up: 'sign_up' }
+        password: 'forgot-password', confirmation: 'verification', sign_up: 'sign_up' },
+        controllers: { registrations: 'users/registrations' }
 
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
